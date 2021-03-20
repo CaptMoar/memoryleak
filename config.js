1ba52dbd8b6e3795e283b7bfddf01618c0fdddec
Sebastian Diaz <sebastian.diazg@outlook.com>
2021-03-20 13:44:14 -0300

    Se agrega SQLite 3 con insersion por event

---
+++ config.js
@@ -0,0 +1,7 @@
+let config = {};
+
+config.MIN_ITERACION=1
+config.MIN_DISCONNECT=30
+config.DB_PATH="./toctoc.db"
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,8 +1,60 @@
 'use strict';
+let { totalmem, freemem, hostname } = require('os');
+let events = require('events');
+let moment = require('moment');
+let sqlite3 = require('sqlite3').verbose();
+let config = require('./config.js');
 
-import { totalmem, freemem } from 'os';
-import moment, { format } from 'moment';
+let eventEmitter = new events.EventEmitter();
+let db;
 
-console.log(totalmem());
-console.log(freemem())
-console.log(moment)
+let connect = () => {
+    db = new sqlite3.Database(config.DB_PATH, sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log(`Connected to the ${config.DB_PATH} database.`);
+    });
+};
+
+let disconnect = () => {
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
+};
+
+let addMetric = () => {
+    let fecha = moment().format('YYYY/MM/DD');
+    let hora = moment().format('HH:mm:ss');
+    let usageMemory = totalmem() - freemem();
+
+    let sql = `INSERT INTO METRICAS (hostname, totalMemory, usageMemory, freeMemory, fecha, hora) VALUES (?,?,?,?,?,?)`
+    let params =  [hostname(), totalmem(), usageMemory, freemem(), fecha, hora] 
+
+    db.all(sql, params ,(err) => {
+        if (err) throw err.message;
+        //console.log(`insertando filas`);
+    });
+};
+
+let removeMetric = () => console.log('removeMetric');
+
+eventEmitter.addListener('connect', connect);
+eventEmitter.addListener('disconnect', disconnect);
+eventEmitter.addListener('addMetric', addMetric);
+eventEmitter.addListener('removeMetric', removeMetric);
+
+eventEmitter.emit('connect');
+
+let interval = setInterval(() => {
+    eventEmitter.emit('addMetric');
+}, 1000);
+
+let handler = setTimeout(() => {
+    clearInterval(interval);
+}, eventEmitter.emit('disconnect') , config.MIN_DISCONNECT * 1000 * 60);
+
+
